5abe8bfb6e8eb98701b530b7acb5e48e2ca2a8fc
SK Dutta <skd@officeclip.com>
2020-07-05 22:21:27 -0400

    multi line graph

--- testChartJs/Util.cs
+++ testChartJs/Util.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Web;
 
 namespace testChartJs
 {
     public static class Util
     {
-        public static List<string> GetColors()
+        public static List<string> GetColors(int i)
         {
-            return new List<string>
+            var colors = new List<string>
                             {
                                 "rgba(255, 99, 132, 0.8)",
                                 "rgba(54, 162, 235, 0.8)",
@@ -19,6 +20,7 @@ namespace testChartJs
                                 "rgba(153, 102, 255, 0.8)",
                                 "rgba(255, 159, 64, 0.8)"
                             };
+            return new List<string>() { colors[i] };
         }
 
         public static List<string> CreateLabels(int minValue, int length)
@@ -33,7 +35,8 @@ namespace testChartJs
 
         public static List<int> CreateRandomInts(int length, int maxValue)
         {
-            var rand = new Random();
+            Thread.Sleep(10);
+            var rand = new Random((int)DateTime.Now.Ticks);
             var rtnlist = new List<int>();
 
             for (int i = 0; i < length; i++)
@@ -41,12 +44,6 @@ namespace testChartJs
                 rtnlist.Add(rand.Next(maxValue));
             }
             return rtnlist;
-            //Random rand = new Random();
-            //var ints = Enumerable.Range(0, length)
-            //                             .Select(i => new Tuple<int, int>(rand.Next(length), i))
-            //                             .OrderBy(i => i.Item1)
-            //                             .Select(i => i.Item2);
-            //return ints.ToList();
         }
     }
 }
