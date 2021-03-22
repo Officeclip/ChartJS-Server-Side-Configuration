5f9b588e07b42e6eb035d7eba35d73ed4fd26584
SK Dutta <skd@officeclip.co>
2021-03-22 15:50:22 -0400

    Commiting after a long time!

--- libChart/DataSetItem.cs
+++ libChart/DataSetItem.cs
@@ -8,7 +8,7 @@ namespace ChartServerConfiguration.Model
     public class DataSetItem
     {
         public string Label { get; set; }
-        public List<int> Data { get; set; }
+        public List<int?> Data { get; set; }
         public int BorderWidth { get; set; }
         public List<string> BackgroundColor { get; set; }
         public string BorderColor { get; set; }
